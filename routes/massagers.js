a4d044c4b6aa19b6c72215685c945999df1071d8
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-09-16 13:01:58 +0300

    Update massagers.js

--- routes/massagers.js
+++ routes/massagers.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const {
-  getMassagers,
-  getMassager,
-  getAvailability,
-  createUpdateProfile,
-  uploadPhotos
-} = require('../controllers/massagerController');
-const { protect, authorize } = require('../middleware/auth');
-const upload = require('../middleware/upload');
+const { getMassagers, getMassager, searchMassagers } = require('../controllers/massagerController');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/', getMassagers);
+router.get('/search', searchMassagers);
 router.get('/:id', getMassager);
-router.get('/:id/availability', getAvailability);
-
-// Protected routes
-router.post('/profile', protect, authorize('massager'), createUpdateProfile);
-router.put('/profile/photos', protect, authorize('massager'), upload.array('photos', 5), uploadPhotos);
 
 module.exports = router;
