bd91c236f9966f56d9dba0c50abbc38aec4c76a5
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-09-16 13:01:31 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const {
-  register,
-  login,
-  getMe,
-  updateDetails,
-  updatePassword
-} = require('../controllers/authController');
-const { protect } = require('../middleware/auth');
+const { register, login, getMe } = require('../controllers/authController');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', protect, getMe);
-router.put('/updatedetails', protect, updateDetails);
-router.put('/updatepassword', protect, updatePassword);
+router.get('/me', auth, getMe);
 
 module.exports = router;
