95acde455f65abe889d421c3e25e31842638858f
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-09-17 12:29:10 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,36 +1,24 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-// Protect routes
-exports.protect = async (req, res, next) => {
+const protect = async (req, res, next) => {
   try {
     let token;
-
+    
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
       token = req.headers.authorization.split(' ')[1];
     }
-
+    
     if (!token) {
       return res.status(401).json({
         success: false,
         message: 'Not authorized to access this route'
       });
     }
-
+    
     try {
-      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      
-      // Get user from token
-      req.user = await User.findById(decoded.id).select('-password');
-      
-      if (!req.user) {
-        return res.status(401).json({
-          success: false,
-          message: 'Not authorized to access this route'
-        });
-      }
-      
+      req.user = await User.findById(decoded.id);
       next();
     } catch (error) {
       return res.status(401).json({
@@ -39,15 +27,14 @@ exports.protect = async (req, res, next) => {
       });
     }
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       success: false,
       message: 'Server Error'
     });
   }
 };
 
-// Grant access to specific roles
-exports.authorize = (...roles) => {
+const authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
@@ -58,3 +45,5 @@ exports.authorize = (...roles) => {
     next();
   };
 };
+
+module.exports = { protect, authorize };
