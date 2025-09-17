626fcc0a34484fcf5ecfc6c66be30e9880dba695
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-09-17 10:31:09 +0300

    Update massagers.js

--- routes/massagers.js
+++ routes/massagers.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { getMassagers, getMassager, searchMassagers } = require('../controllers/massagerController');
-const auth = require('../middleware/auth');
+const {
+  getMassagers,
+  getMassager,
+  getMassagerAvailability,
+  searchMassagers
+} = require('../controllers/massagerController');
 
 const router = express.Router();
 
 router.get('/', getMassagers);
 router.get('/search', searchMassagers);
 router.get('/:id', getMassager);
+router.get('/:id/availability', getMassagerAvailability);
 
 module.exports = router;
